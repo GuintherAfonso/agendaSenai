2840b42cf24c40d36c93dcbe0800bc7121805656
GuintherAfonso <guintherumaga@hotmail.com>
2022-11-30 11:23:52 -0300

    sexto commit

--- excluir_contato.php
+++ excluir_contato.php
@@ -1,12 +1,13 @@
 <?php
-include 'classe/contatos.class.php';
-$contato = new COntatos();
+include 'classes/contatos.class.php';
+$contato = new Contatos();
 
 if(!empty($GET['id'])){
     $id = $GET['id'];
     $contato->excluir($id);
     header("Location /agendaSenai");
 }else{
-    echo '<scrip type="text/javascript">alert("Erro ao excluir contato!");</script>';
+    echo '<scrip type="text/javascript">alert("Erro ao excluir contato!")</script>';
     header("Location /agendaSenai");
 }
+?>
